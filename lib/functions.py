dd320b5a0b2ca4e160b0eaf7bc03da1b8cdc6599
sumeyaahmed98 <sumeyaahmed@gmail.com>
2025-06-04 15:32:23 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
-
+# 1. greet_programmer
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# 2. greet
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# 3. greet_with_default
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# 4. add
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# 5. halve
 def halve(number):
-    pass
+    return number / 2
